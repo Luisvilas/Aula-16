23a04d3403223a865af47b4770aa6f1222459ec7
luis <luisvboas@outlook.com>
2024-12-04 21:09:56 -0300

    aula 16

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header(){
+    return(
+        <div>
+            <h1>Hello!</h1>
+            <p>This works!</p>
+        </div>
+    );
+}
+
+export default Header;
